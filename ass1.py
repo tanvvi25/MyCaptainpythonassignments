9f0d759e4b66373b9ae50131077b8cc0a209a81f
tanvvi25 <137600219+tanvvi25@users.noreply.github.com>
2023-06-26 13:14:48 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from math import pi
+r = float(input ("Input the radius of the circle :1.1 "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
