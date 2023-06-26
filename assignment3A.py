d3690781d4016b8c56473e3167466ed11494af22
tanvvi25 <137600219+tanvvi25@users.noreply.github.com>
2023-06-26 13:41:38 +0530

    Add files via upload

---
+++ assignment3A.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 13:34:04 2023
+
+@author: USER
+"""
+
+n = int(input("Enter length of Fibonacci series: "))
+num1 = 0
+num2 = 1
+next_number = 0
+count = 1
+
+while(count <= n):
+	print(next_number, end=" ")
+	count += 1
+	num1 = num2
+	num2 = next_number
+	next_number = num1 + num2
+	t_number = num1 + num2
